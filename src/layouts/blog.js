ddb5a1f259047051d374bf6183da95502fd61271
tricinel <bogdan@bogdanlazar.com>
2018-03-14 15:55:23 +0100

    Finish layouts

--- src/layouts/blog.js
+++ src/layouts/blog.js
@@ -4,11 +4,13 @@ import Helmet from 'react-helmet';
 import './index.css';
 
 import Wrapper from '../components/Layout';
+import FooterLinks from '../components/Footer';
 
 const TemplateWrapper = ({ children }) => (
   <Wrapper>
     <Helmet title="BogdanLazar.com" />
     {children()}
+    <FooterLinks />
   </Wrapper>
 );
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,38 +3,14 @@ import Helmet from 'react-helmet';
 
 import './index.css';
 
-import Wrapper, { Footer } from '../components/Layout';
+import Wrapper from '../components/Layout';
+import FooterLinks from '../components/Footer';
 
 const TemplateWrapper = ({ children }) => (
   <Wrapper>
     <Helmet title="BogdanLazar.com" />
     {children()}
-
-    <Footer>
-      <h3>Get in touch</h3>
-      <ul>
-        <li>
-          <a href="https://github.com/tricinel" title="Github">
-            Github
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/tricinel" title="LinkedIn">
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/tricinel" title="Twitter">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#98;&#111;&#103;&#100;&#97;&#110;&#64;&#98;&#111;&#103;&#100;&#97;&#110;&#108;&#97;&#122;&#97;&#114;&#46;&#99;&#111;&#109;">
-            Let&apos;s talk!
-          </a>
-        </li>
-      </ul>
-    </Footer>
+    <FooterLinks />
   </Wrapper>
 );
 
